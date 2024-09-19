ec41d11c74a502c7eab2204f8695cb1bc3d195a5
Luis Luna <101300840+LuisDeLuna-Dev@users.noreply.github.com>
2024-09-19 16:34:00 -0300

    feat: implementa animação ao scroll

--- script.js
+++ script.js
@@ -40,22 +40,44 @@ function initAccordion() {
 }
 initAccordion();
 
-function initScrollSuave(){
+function initScrollSuave() {
 
     const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-    function scrollToSection(event){
+    function scrollToSection(event) {
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
         section.scrollIntoView({
-            behavior:'smooth',
+            behavior: 'smooth',
             block: 'start'
         });
     }
-    
+
     linksInternos.forEach((link) => {
         link.addEventListener('click', scrollToSection);
     })
 }
 initScrollSuave();
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function initAnimacaoScroll() {
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+            })
+        }
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+initAnimacaoScroll();
